3ed3340bf2fa5defe744da9d2f0d59c6ffbee7fd
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-10 11:06:56 -0700

    Solution at end of 35 minutes
    Was attempting to improve solution, since run time was about O(n^3)

--- Medium/3Sum.java
+++ Medium/3Sum.java
@@ -1,8 +1,60 @@
 package Medium;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 class problem3Sum{
 
     public static void main(String[] args) {
         
+        System.out.println("hello world");
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        ArrayList<List<Integer>> sol = new ArrayList<>();
+        
+        int left = 0;
+        
+        while(left < nums.length-2)
+        {
+            if(nums[left] == nums[left+1])
+                left++;
+            else
+            {
+                int innerLeft = left+1;
+                int innerRight = innerLeft+1;
+                
+                while(innerRight < nums.length)
+                {
+                    if(nums[innerLeft] == nums[innerRight])
+                    {
+                        innerLeft = innerRight;
+                        innerRight++;
+                    }
+                    else
+                    {
+                        if(nums[left]+nums[innerLeft]+nums[innerRight] == 0)
+                        {
+                            ArrayList<Integer> temp = new ArrayList<>();
+                            temp.add(nums[left]);
+                            temp.add(nums[innerLeft]);
+                            temp.add(nums[innerRight]);
+                            sol.add(temp);
+                            
+                            innerLeft = innerRight;
+                            innerRight++;
+                        }
+                    }
+                }
+                
+                left++;
+            }
+        }
+        
+        return sol;
     }
 }
