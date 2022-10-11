4f8f530895d2008120eee5e2a1510afbb1f71fd6
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-11 11:27:27 -0700

    Faster and elegant solution found

--- Medium/235 Lowest Common Ancestor of a BS Tree.java
+++ Medium/235 Lowest Common Ancestor of a BS Tree.java
@@ -1,8 +1,5 @@
 package Medium;
 
-import java.util.Queue;
-import java.util.LinkedList;
-
 class problem235{
 
     public static void main(String[] args) {
@@ -12,55 +9,18 @@ class problem235{
 
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        //Left node and right node to traverse the trees
-        TreeNode left = root;
-        TreeNode right = root;
-        TreeNode sol = root;
+                
+        int small = Math.min(p.val, q.val);
+        int large = Math.max(p.val, q.val);
         
-        //Queues to keep track of pathway taken to reach nodes
-        Queue<TreeNode> leftParents = new LinkedList<>();
-        Queue<TreeNode> rightParents = new LinkedList<>();
-        
-        //Keep looping until we found the values we are searching for
-        while(left.val != p.val && right.val != q.val)
-        {
-            //Add both of them to the queue, since its own node can equal itself
-            leftParents.add(left);
-            rightParents.add(right);
-            
-            
-            //Left node w/ p
-            if(left.val > p.val)
-            {
-                left = left.left;
-            }
-            else if(left.val < p.val)
-            {
-                left = left.right;
-            }
-            
-            //Right node w/ q
-            if(right.val > q.val)
-            {
-                right = right.left;
-            }
-            else if(right.val < q.val)
-            {
-                right = right.right;
-            }
-            
+        while (root != null) {
+            if (root.val > large)
+                root = root.left;
+            else if (root.val < small)
+                root = root.right;
+            else
+                return root;
         }
-        
-        //We found the nodes for both, so now we keep looping until we find a node
-        //where they don't match up, and we should have found our solution
-        //Error in logic somewehre in code, because incorrect node is returned
-        while(leftParents.peek() != rightParents.peek())
-        {
-            sol = leftParents.poll();
-            rightParents.poll();
-        }
-        
-        
-        return sol;
+        return null;
     }
 }
