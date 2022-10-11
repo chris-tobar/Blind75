f185af65a2d7a28d0d3820e92244212a4abc3309
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-11 11:36:38 -0700

    Comments explaining more elegant solution

--- Medium/235 Lowest Common Ancestor of a BS Tree.java
+++ Medium/235 Lowest Common Ancestor of a BS Tree.java
@@ -9,15 +9,20 @@ class problem235{
 
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-                
+        //
         int small = Math.min(p.val, q.val);
         int large = Math.max(p.val, q.val);
         
+        //Keep iterating until we found our solution, or the nodes doesn't exist
         while (root != null) {
+
+            //Both nodes exist in the left subtree
             if (root.val > large)
                 root = root.left;
+            //Both nodes exist in the right subtree
             else if (root.val < small)
                 root = root.right;
+            //Current node is between the two values found
             else
                 return root;
         }
