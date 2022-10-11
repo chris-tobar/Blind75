0099e507121e0d965b1ff7d54a6e35ac5e3b229e
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-11 11:04:30 -0700

    Code achieved within 35 minutes
    Timer ran out with this solution. Idea was to keep track of path going down, and once the paths no longer matched, that means we found the LCA of the parents.

--- Medium/235 Lowest Common Ancestor of a BS Tree.java
+++ Medium/235 Lowest Common Ancestor of a BS Tree.java
@@ -1,9 +1,59 @@
 package Medium;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 class problem235{
 
     public static void main(String[] args) {
         
         System.out.println("Hello world!");
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        TreeNode left = root;
+        TreeNode right = root;
+        TreeNode sol = root;
+        
+        Queue<TreeNode> leftParents = new LinkedList<>();
+        Queue<TreeNode> rightParents = new LinkedList<>();
+        
+        while(left.val != p.val && right.val != q.val)
+        {
+            leftParents.add(left);
+            rightParents.add(right);
+            
+            
+            //Left node w/ p
+            if(left.val > p.val)
+            {
+                left = left.left;
+            }
+            else if(left.val < p.val)
+            {
+                left = left.right;
+            }
+            
+            //Right node w/ q
+            if(right.val > q.val)
+            {
+                right = right.left;
+            }
+            else if(right.val < q.val)
+            {
+                right = right.right;
+            }
+            
+        }
+        
+        while(leftParents.peek() != rightParents.peek())
+        {
+            sol = leftParents.poll();
+            rightParents.poll();
+        }
+        
+        
+        return sol;
     }
 }
---
+++ Medium/TreeNode.java
@@ -0,0 +1,9 @@
+package Medium;
+
+public class TreeNode {
+    
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
