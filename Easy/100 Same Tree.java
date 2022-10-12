474da53aaf5d7ed18217fd3eb3eccaa83b50dd53
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-12 11:25:50 -0700

    Solution achieved in 4 minutes

--- Easy/100 Same Tree.java
+++ Easy/100 Same Tree.java
@@ -5,4 +5,18 @@ class problem100{
     public static void main(String[] args) {
         
     }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p == null && q!=null)
+            return false;
+        else if(p != null && q==null)
+            return false;
+        else if(p==null && q==null)
+            return true;
+        else if(p.val != q.val)
+            return false;
+        
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
 }
