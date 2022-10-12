97684d4379a84e0e71978e1c0c5d9246cbca8ba8
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-12 11:27:59 -0700

    Comments explaining code
    Time complexity: O(n) where n is the number of nodes within the tree
    
    Space Complexity: O(n) or O(1)
    If considered recursion as stack space, then n is the height of the tree traversed, else its constant time

--- Easy/100 Same Tree.java
+++ Easy/100 Same Tree.java
@@ -8,15 +8,19 @@ class problem100{
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
         
+        //Base cases to check
         if(p == null && q!=null)
             return false;
         else if(p != null && q==null)
             return false;
+        //Both nodes have reached a leaf node, thus making them the same
         else if(p==null && q==null)
             return true;
+        //Both nodes have a child, but they are not the same, so early return
         else if(p.val != q.val)
             return false;
         
+        //Both the left and right side need to be true, in order for the WHOLE Tree to be true
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
