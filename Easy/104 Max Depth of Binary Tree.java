785b9cd7c29c318e3600f24169b65493b12f4ff1
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-12 11:19:14 -0700

    Problem description added

--- Easy/104 Max Depth of Binary Tree.java
+++ Easy/104 Max Depth of Binary Tree.java
@@ -1,6 +1,11 @@
 package Easy;
 
+/*
+ * Given the root of a binary tree, return its maximum depth.
 
+    A binary tree's maximum depth is the number of nodes along 
+    the longest path from the root node down to the farthest leaf node.
+ */
 class problem104{
     
     public static void main(String[] args) {
