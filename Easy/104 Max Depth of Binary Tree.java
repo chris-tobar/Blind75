61566e1cfc4b429c09693a8e00b86f060a201005
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-12 11:16:36 -0700

    Comments explaining solution
    Time complexity: O(n) where n is number of nodes within the tree
    
    Space complexity: O(1) since we used recursion to solve the problem

--- Easy/104 Max Depth of Binary Tree.java
+++ Easy/104 Max Depth of Binary Tree.java
@@ -7,19 +7,27 @@ class problem104{
         
     }
 
+    
     public static int maxDepth(TreeNode root) {
         
+        //Use the helper function, starting a depth of 0
+        //if the node we currently traversed is null, then we have a depth of 0
         return DFS(root, 0);
     }
     
+    //Helper function that traverses the tree
     public static int DFS(TreeNode root, int depth)
     {
+        //Node visiting is null, so we return the current depth that we found
         if(root == null)
             return depth;
         
+        //Iterate on the left node
         int left = DFS(root.left, depth+1);
+        //Iterate on the right node
         int right = DFS(root.right, depth+1);
         
+        //Return the max of the two found
         return Math.max(left,right);
         
     }
