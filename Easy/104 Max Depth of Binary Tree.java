d62dac8ba0e05aa7790044f734f666fc69cefd16
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-12 11:14:07 -0700

    Solution achieved in 2 minutes

--- Easy/104 Max Depth of Binary Tree.java
+++ Easy/104 Max Depth of Binary Tree.java
@@ -1,8 +1,26 @@
 package Easy;
 
+
 class problem104{
     
     public static void main(String[] args) {
+        
+    }
+
+    public static int maxDepth(TreeNode root) {
+        
+        return DFS(root, 0);
+    }
+    
+    public static int DFS(TreeNode root, int depth)
+    {
+        if(root == null)
+            return depth;
+        
+        int left = DFS(root.left, depth+1);
+        int right = DFS(root.right, depth+1);
+        
+        return Math.max(left,right);
         
     }
 }
